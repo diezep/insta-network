d82e225e0a646c686f053930f89f1fa73752710a
Diego Zepeda <diegomzepeda11@gmail.com>
2020-01-31 01:10:23 -0600

    Proyecto funcional.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Insta-network',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/diezep/insta-network/',
+    license='MIT',
+    author='Diezep',
+    author_email='diegomzepeda11@gmail.com',
+    description=''
+)
