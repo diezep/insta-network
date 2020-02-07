b3b9593afc879de37599bf7f5e8676d567adc4c9
Diego Zepeda <diegomzepeda11@gmail.com>
2020-02-07 01:27:12 -0600

    Min fix

--- User.py
+++ User.py
@@ -1,10 +1,10 @@
 class User:
     tipo = ""
+    descripcion = ''
 
-    def __init__(self, nombre, usuario, publicaciones, seguidores, seguidos, descripcion):
+    def __init__(self, nombre, usuario, publicaciones, seguidores, seguidos):
         self.nombre = nombre
         self.usuario = usuario
         self.publicaciones = publicaciones
         self.seguidores = seguidores
         self.seguidos = seguidos
-        self.descripcion = descripcion
